7c94a2a9e22287ee61e7ff065e303bf004d3ed6e
Aditya Gupta <adityag860libra@gmail.com>
2025-05-12 05:24:08 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -38,7 +38,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: process.env.NODE_ENV === 'production',
+        secure: true,
         httpOnly: true,                               
         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         maxAge: 24 * 60 * 60 * 1000
