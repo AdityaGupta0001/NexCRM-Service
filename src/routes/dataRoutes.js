7e11ceb5f3371ea2066747227ef55ee2cc4971b5
Aditya Gupta <adityag860libra@gmail.com>
2025-05-12 04:17:30 +0530

    Update dataRoutes.js

--- src/routes/dataRoutes.js
+++ src/routes/dataRoutes.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 const dataIngestionController = require('../controllers/dataIngestionController');
 const { isAuthenticated } = require('../middlewares/authMiddleware');
-const { isAdmin } = require('../middlewares/roleMiddleware');
+const { isAdmin, isEmployeeOrAdmin } = require('../middlewares/roleMiddleware');
 
 // Admin-only access to upload customer data
 router.post('/customers', isAuthenticated, isAdmin, dataIngestionController.uploadCustomers);
+router.get('/customers', isAuthenticated, isEmployeeOrAdmin, dataIngestionController.getAllCustomers);
 
 // Admin-only access to upload order data
 router.post('/orders', isAuthenticated, isAdmin, dataIngestionController.uploadOrders);
+router.get('/orders', isAuthenticated, isEmployeeOrAdmin, dataIngestionController.getAllOrders);
 
 module.exports = router;
